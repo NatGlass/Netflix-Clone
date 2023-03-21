fcb7ba1f641e9b906f43f2b265954efb873f8075
Nathan G <Nathanglass26@gmail.com>
2023-03-21 19:58:48 +0000

    netflix clone

---
+++ pages/watch/[movieId].tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import useMovie from "@/hooks/useMovie";
+import { useRouter } from "next/router";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
+const Watch = () => {
+    const router = useRouter()
+    const { movieId } = router.query
+
+    const { data } = useMovie(movieId as string)
+
+    return (
+        <div className="h-screen w-screen bg-black">
+            <nav className="
+            fixed
+            w-full
+            p-4
+            z-10
+            flex
+            flex-row
+            items-center
+            gap-8
+            bg-black/70
+            ">
+                <AiOutlineArrowLeft onClick={() => router.push('/')} className="text-white cursor-pointer" size={30} />
+                <p className="text-white text-1xl md:text-2xl font-bold">
+                    {data?.title}
+                </p>
+            </nav>
+            <video src={data?.videoUrl} autoPlay controls className="h-full w-full">
+
+            </video>
+        </div>
+    )
+}
+
+export default Watch
